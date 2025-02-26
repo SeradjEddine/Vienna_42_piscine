a28520273b22b3ceabd2edfd32519deb595625c9
SeradjEddine <medsiradjedinne.fekhar@gmail.com>
2025-02-26 01:43:50 +0100

    na

--- c04/ex05/ft_atoi_base.c
+++ c04/ex05/ft_atoi_base.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 
 int check_base(char *b, int *len)
 {
@@ -62,49 +63,102 @@ int numfy (char *base, int base_len, char *str, int start)
 
 	return (num);
 }
+void swap (char *str, int end)
+{
+	char	tmp;
+	int	start;
 
-int ft_atoi_base(char *str, char *base)
+	if (str[0] == '-')
+		start = 1;
+	else
+		start = 0;
+	while (start < end)
+	{
+		tmp = str [start];
+		str [start] = str [end];
+		str [end] = tmp;
+		start ++;
+		end --;
+	}
+}
+
+char *strfy(int num, char *base_to, int base_to_len, int sign)
+{
+	int	i;
+	int	size;
+	int 	tmp;
+	char	*mystring;
+
+
+	size = 0;
+	tmp = num;
+	while (tmp)
+	{
+		tmp /= base_to_len;
+		size ++;
+	}
+	if (sign == 1)
+		mystring = malloc (size + 1);
+	else
+		mystring = malloc (size + 2);
+	if (!mystring)
+		return (0);
+	i = 0;
+	if (sign == -1)
+		mystring [i++] = '-';
+
+	while (num > 0)
+	{
+		mystring[i++] = base_to[(num % base_to_len)];
+		num /= base_to_len;
+        }
+	swap (mystring, i-1);	
+	mystring[i] = '\0';
+	return (mystring);
+}
+
+
+char *ft_convert_base(char *nbr, char *base_from, char *base_to)
 {	
-	int base_len;
-	int start;
-	int sign;
-	int num;
+	char	*converted;
+	int	base_from_len;
+	int	base_to_len;
+	int	start;
+	int	sign;
+	int	num;
 
 	sign = 1;
-	base_len = 0;
-	if (!check_base(base, &base_len))
+	if (!check_base(base_from, &base_from_len) || (!check_base(base_to, &base_to_len)))
 		return 0;
-	start = preproccess (str, &sign);
-	num = numfy(base, base_len, str, start);
-	num *= sign;
-	return (num);	
+	start = preproccess (nbr, &sign);
+	num = numfy(base_from, base_from_len, nbr, start);
+	if (num != 0)
+		converted = strfy( num, base_to, base_to_len, sign);
+	else
+	{
+		converted = malloc (2);
+		if (!converted)
+			return (0);
+		converted [0] = base_to [0];
+		converted [1] = '\0';
+	}
+	return (converted);	
 }
 
 #include <stdio.h>
 
-int main()
-{
-    // Test cases
-    printf("Test 1: \"1010\" (binary) -> %d\n", ft_atoi_base("1010", "01")); // Expected: 10
-    printf("Test 2: \"-1010\" (binary) -> %d\n", ft_atoi_base("-1010", "01")); // Expected: -10
-    printf("Test 3: \"7F\" (hex) -> %d\n", ft_atoi_base("7F", "0123456789ABCDEF")); // Expected: 127
-    printf("Test 4: \"  +--42\" (decimal) -> %d\n", ft_atoi_base("  +--42", "0123456789")); // Expected: 42
-    printf("Test 5: \"  -+42\" (invalid base) -> %d\n", ft_atoi_base("  -+42", "0123456789")); // Expected: -42 
-    printf("Test 6: \"123\" (base 4) -> %d\n", ft_atoi_base("123", "0123")); // Expected: 27
-    printf("Test 7: \"123\" (invalid base) -> %d\n", ft_atoi_base("123", "01123")); // Expected: 0 (invalid base)
-    printf("Test 8: \"123\" (empty base) -> %d\n", ft_atoi_base("123", "")); // Expected: 0 (invalid base)
-    printf("Test 9: \"123\" (base size 1) -> %d\n", ft_atoi_base("123", "0")); // Expected: 0 (invalid base)
-    printf("Test 10: \"123\" (base with whitespace) -> %d\n", ft_atoi_base("123", "0123 ")); // Expected: 0 (invalid base)
-    printf("Test 11: \"123\" (base with duplicates) -> %d\n", ft_atoi_base("123", "01234")); // Expected: 0 (invalid base)
-    printf("Test 12: \"123\" (base with +) -> %d\n", ft_atoi_base("123", "01234+")); // Expected: 0 (invalid base)
-    printf("Test 13: \"123\" (base with -) -> %d\n", ft_atoi_base("123", "01234-")); // Expected: 0 (invalid base)
-    printf("Test 14: \"123\" (base with tab) -> %d\n", ft_atoi_base("123", "01234\t")); // Expected: 0 (invalid base)
-    printf("Test 15: \"123\" (base with newline) -> %d\n", ft_atoi_base("123", "01234\n")); // Expected: 0 (invalid base)
-    printf("Test 16: \"123\" (base with carriage return) -> %d\n", ft_atoi_base("123", "01234\r")); // Expected: 0 (invalid base)
-    printf("Test 17: \"123\" (base with vertical tab) -> %d\n", ft_atoi_base("123", "01234\v")); // Expected: 0 (invalid base)
-    printf("Test 18: \"123\" (base with form feed) -> %d\n", ft_atoi_base("123", "01234\f")); // Expected: 0 (invalid base)
-    printf("Test 19: \"123\" (base with all invalid chars) -> %d\n", ft_atoi_base("123", "+- \t\n\r\v\f")); // Expected: 0 (invalid base)
-    printf("Test 20: \"123\" (base with valid chars) -> %d\n", ft_atoi_base("123", "0123456789")); // Expected: 123
+int main() {
+    // Test 1: Binary to Decimal
+    printf("%s\n", ft_convert_base("+---+1013142kjhid10", "234k01", "0123456789")); // "10"
+
+    // Test 2: Negative Decimal to Hex
+    printf("%s\n", ft_convert_base("-42", "0123456789", "0123456789ABCDEF")); // "-2A"
+
+    // Test 3: Zero Case
+    printf("%s\n", ft_convert_base("0", "01", "0123456789")); // "0"
+
+    // Test 4: Invalid Base
+    printf("%s\n", ft_convert_base("123", "012", "01")); // NULL
 
     return 0;
 }
